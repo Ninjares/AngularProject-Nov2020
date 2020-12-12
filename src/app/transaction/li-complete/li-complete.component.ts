765eecfc3858980719c94048f69d9d997cc89bb8
Daniel <ninjares@abv.bg>
2020-12-12 18:33:26 +0200

    li-complete sylizing

--- src/app/transaction/li-complete/li-complete.component.ts
+++ src/app/transaction/li-complete/li-complete.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { TxComplete } from '../models/TxCModel';
 
 @Component({
@@ -10,8 +11,16 @@ export class LiCompleteComponent implements OnInit {
 
   viewDetails:boolean = false;
   @Input() transaction: TxComplete;
-  constructor() { }
+  atUserPage: boolean = false;
+  merchantField:string;
+  constructor(private router: Router, private route: ActivatedRoute) { }
   ngOnInit(): void {
+    this.atUserPage = /\/user\/([\w\/]*)/.test(this.router.url);
+    if(this.atUserPage) {
+      let user = /\/user\/([\w\/]*)/.exec(this.router.url)[1];
+      if(this.transaction.buyer==user) this.merchantField=this.transaction.seller;
+      else this.merchantField = this.transaction.buyer;
+    }
   }
   
   toggleDetails(){
