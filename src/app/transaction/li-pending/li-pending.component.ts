e1e6146ae2a6a28bda9007ef25953ec08f6021cb
Daniel <ninjares@abv.bg>
2020-12-13 09:42:57 +0200

    pendingTx at userpage

--- src/app/transaction/li-pending/li-pending.component.ts
+++ src/app/transaction/li-pending/li-pending.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Router } from '@angular/router';
 import { TxModel } from '../models/TxModel';
 
 @Component({
@@ -11,8 +12,10 @@ export class LiPendingComponent implements OnInit {
   viewDetails:boolean = false;
   @Input() transaction: TxModel;
   @Output() refreshpage = new EventEmitter();
-  constructor() { }
+  atUserPage: boolean = false;
+  constructor(private router: Router) { }
   ngOnInit(): void {
+    this.atUserPage = /\/user\/([\w\/]*)/.test(this.router.url);
   }
   refresh(){
     this.refreshpage.emit();
